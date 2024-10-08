1c2d350cd429bfdfe98f9f0a633f1ec6ed3a31d8
Karan Negi <btwitskaranhere@gmail.com>
2024-10-08 18:58:40 +0530

    api

--- src/components/black-jack/Dashboard.jsx
+++ src/components/black-jack/Dashboard.jsx
@@ -19,12 +19,53 @@ import Form from 'react-bootstrap/Form';
 import menu from '../assests/menu.png'
 import { MdAddToPhotos } from "react-icons/md";
 import caraousel from '../assests/caraousel.png'
-
+import axios from 'axios';
 
 
 
 
 const Dashboard = () => {
+
+
+  const [restaurantDetails, setRestaurantDetails] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setRestaurantDetails({ ...restaurantDetails, [name]: value });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzA0ZmZjYTMyNWZjMjhiNzQ3M2E5NmIiLCJpYXQiOjE3MjgzODEzNzR9.6GyQCpxiKzrER0o46qhSIAIiJWYByaLKCkyPWIvjmdc';
+
+      const response = await axios.post('https://tv-app-cloud-backend.onrender.com/restaurant/add', restaurantDetails,
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+      console.log(response.data);
+      setSuccessMessage('Success fgg');
+
+
+
+    } catch (error) {
+      setErrorMessage('Failed to add restaurant. Please try again.');
+      
+      console.log("failed")
+    }
+  };
+
   const [selectedItem, setSelectedItem] = useState('Dashboard');
 
   const [openSubMenu, setOpenSubMenu] = useState(null); // To track which submenu is open
@@ -180,21 +221,45 @@ const Dashboard = () => {
             <div>
               <div className="wrapper">
                 <h2>Add Your Restaurant</h2>
-                <form action="#">
+                <form onSubmit={handleFormSubmit}>
                   <div className="input-box">
-                    <input type="text" placeholder="Restaurant Name" required />
+                    <input
+                      type="text"
+                      placeholder="Restaurant Name"
+                      name="name"
+                      value={restaurantDetails.name}
+                      onChange={handleInputChange}
+                      required
+                    />
                   </div>
                   <div className="input-box">
-                    <input type="email" placeholder="Email" required />
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      value={restaurantDetails.email}
+                      onChange={handleInputChange}
+                      required
+                    />
                   </div>
                   <div className="input-box">
-                    <input type="password" placeholder="Password" required />
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={restaurantDetails.password}
+                      onChange={handleInputChange}
+                      required
+                    />
                   </div>
 
                   <div className="input-box button">
                     <input type="submit" value="Add Restaurant" />
                   </div>
                 </form>
+                {successMessage && <p className="success-message">{successMessage}</p>}
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+
               </div>
             </div>
           )}
